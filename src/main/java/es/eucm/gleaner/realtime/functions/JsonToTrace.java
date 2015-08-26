e5a069f244565578017076887b2de80ec2ad8f39
RotaruDan <drotaru@ucm.es>
2015-08-26 10:50:21 +0200

    Changes JsonToTrace to process xAPI like statements.

--- src/main/java/es/eucm/gleaner/realtime/functions/JsonToTrace.java
+++ src/main/java/es/eucm/gleaner/realtime/functions/JsonToTrace.java
@@ -1,6 +1,9 @@
 package es.eucm.gleaner.realtime.functions;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 import storm.trident.operation.Function;
 import storm.trident.operation.TridentCollector;
@@ -9,15 +12,14 @@ import storm.trident.tuple.TridentTuple;
 
 import java.lang.reflect.Type;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 public class JsonToTrace implements Function {
 
 	private String versionId;
 
-	private Gson gson;
-
-	private Type type;
+	private JsonParser parser;
 
 	public JsonToTrace(String versionId) {
 		this.versionId = versionId;
@@ -25,15 +27,23 @@ public class JsonToTrace implements Function {
 
 	@Override
 	public void execute(TridentTuple tuple, TridentCollector collector) {
-		Object trace = gson.fromJson(tuple.getStringByField("str"), type);
+		String json = tuple.getStringByField("str");
+		JsonObject rootObj = parser.parse(json).getAsJsonObject();
+
+		JsonObject extensionsObject = rootObj.getAsJsonObject("object")
+				.getAsJsonObject("definition").getAsJsonObject("extensions");
+
+		Map<String, String> trace = new HashMap<String, String>();
+		for (Map.Entry<String, JsonElement> jsonElementEntry : extensionsObject.entrySet()) {
+			trace.put(jsonElementEntry.getKey(), jsonElementEntry.getValue().getAsString());
+		}
+
 		collector.emit(Arrays.asList(versionId, trace));
 	}
 
 	@Override
 	public void prepare(Map conf, TridentOperationContext context) {
-		gson = new Gson();
-		type = new TypeToken<Map<String, Object>>() {
-		}.getType();
+		parser = new JsonParser();
 	}
 
 	@Override
--- src/main/java/es/eucm/gleaner/realtime/topologies/RealtimeTopology.java
+++ src/main/java/es/eucm/gleaner/realtime/topologies/RealtimeTopology.java
@@ -14,77 +14,80 @@ import storm.trident.state.StateUpdater;
 
 public class RealtimeTopology extends TridentTopology {
 
-	public <T> void prepare(ITridentSpout<T> spout, StateFactory stateFactory) {
-		prepare(newStream("traces", spout), stateFactory);
-	}
+    public <T> void prepare(ITridentSpout<T> spout, StateFactory stateFactory) {
+        prepare(newStream("traces", spout), stateFactory);
+    }
 
-	/**
-	 * @param traces
-	 *            a stream with two fields: a versionId {@link String} and a
-	 *            trace {@link java.util.Map}
-	 */
-	public void prepare(Stream traces, StateFactory stateFactory) {
+    /**
+     * @param traces a stream with two fields: a versionId {@link String} and a
+     *               trace {@link java.util.Map}
+     */
+    public void prepare(Stream traces, StateFactory stateFactory) {
 
-		StateUpdater stateUpdater = new GameplayStateUpdater();
+        StateUpdater stateUpdater = new GameplayStateUpdater();
 
-		Stream eventStream = createTracesStream(traces).each(
-				new Fields("trace"),
-				new TraceFieldExtractor("gameplayId", "event"),
-				new Fields("gameplayId", "event"));
+        Stream eventStream = createTracesStream(traces).each(
+                new Fields("trace"),
+                new TraceFieldExtractor("gameplayId", "event"),
+                new Fields("gameplayId", "event"));
 
-		// Zones
-		eventStream
-				.each(new Fields("event", "trace"),
-						new FieldValueFilter("event", "zone"))
-				.each(new Fields("trace"), new TraceFieldExtractor("value"),
-						new Fields("value"))
-				.each(new Fields("event", "value"),
-						new PropertyCreator("value", "event"),
-						new Fields("p", "v"))
-				.partitionPersist(stateFactory,
-						new Fields("versionId", "gameplayId", "p", "v"),
-						stateUpdater);
 
-		// Variables
-		eventStream
-				.each(new Fields("event", "trace"),
-						new FieldValueFilter("event", "var"))
-				.each(new Fields("trace"),
-						new TraceFieldExtractor("target", "value"),
-						new Fields("var", "value"))
-				.each(new Fields("event", "var", "value"),
-						new PropertyCreator("value", "event", "var"),
-						new Fields("p", "v"))
-				.partitionPersist(stateFactory,
-						new Fields("versionId", "gameplayId", "p", "v"),
-						stateUpdater);
+        // Zones
+        eventStream
+                .each(new Fields("event", "trace"),
+                        new FieldValueFilter("event", "zone"))
+                .each(new Fields("trace"), new TraceFieldExtractor("value"),
+                        new Fields("value"))
+                .each(new Fields("event", "value"),
+                        new PropertyCreator("value", "event"),
+                        new Fields("p", "v"))
+                .partitionPersist(stateFactory,
+                        new Fields("versionId", "gameplayId", "p", "v"),
+                        stateUpdater);
 
-		// Choices
-		eventStream
-				.each(new Fields("event", "trace"),
-						new FieldValueFilter("event", "choice"))
-				.each(new Fields("trace"),
-						new TraceFieldExtractor("target", "value"),
-						new Fields("choice", "option"))
-				.groupBy(
-						new Fields("versionId", "gameplayId", "event",
-								"choice", "option"))
-				.persistentAggregate(stateFactory, new Count(),
-						new Fields("count"));
 
-		// Interactions
-		eventStream
-				.each(new Fields("event", "trace"),
-						new FieldValueFilter("event", "interact"))
-				.each(new Fields("trace"), new TraceFieldExtractor("target"),
-						new Fields("target"))
-				.groupBy(
-						new Fields("versionId", "gameplayId", "event", "target"))
-				.persistentAggregate(stateFactory, new Count(),
-						new Fields("count"));
-	}
+        // Variables
+        eventStream
+                .each(new Fields("event", "trace"),
+                        new FieldValueFilter("event", "var"))
+                .each(new Fields("trace"),
+                        new TraceFieldExtractor("target", "value"),
+                        new Fields("var", "value"))
+                .each(new Fields("event", "var", "value"),
+                        new PropertyCreator("value", "event", "var"),
+                        new Fields("p", "v"))
+                .partitionPersist(stateFactory,
+                        new Fields("versionId", "gameplayId", "p", "v"),
+                        stateUpdater);
 
-	protected Stream createTracesStream(Stream stream) {
-		return stream;
-	}
+
+        // Choices
+        eventStream
+                .each(new Fields("event", "trace"),
+                        new FieldValueFilter("event", "choice"))
+                .each(new Fields("trace"),
+                        new TraceFieldExtractor("target", "value"),
+                        new Fields("choice", "option"))
+                .groupBy(
+                        new Fields("versionId", "gameplayId", "event",
+                                "choice", "option"))
+                .persistentAggregate(stateFactory, new Count(),
+                        new Fields("count"));
+
+        // Interactions
+        eventStream
+                .each(new Fields("event", "trace"),
+                        new FieldValueFilter("event", "interact"))
+                .each(new Fields("trace"), new TraceFieldExtractor("target"),
+                        new Fields("target"))
+                .groupBy(
+                        new Fields("versionId", "gameplayId", "event", "target"))
+                .persistentAggregate(stateFactory, new Count(),
+                        new Fields("count"));
+
+    }
+
+    protected Stream createTracesStream(Stream stream) {
+        return stream;
+    }
 }
