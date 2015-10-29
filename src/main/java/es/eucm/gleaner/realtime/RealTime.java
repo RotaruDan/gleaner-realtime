9f1c5cfb4ca135b53c8e9ca010b0427ba488b628
RotaruDan <drotaru@ucm.es>
2015-10-29 13:23:35 +0100

    Improves Config object setup.

--- src/main/java/es/eucm/gleaner/realtime/RealTime.java
+++ src/main/java/es/eucm/gleaner/realtime/RealTime.java
@@ -22,24 +22,42 @@ public class RealTime {
 		return kafkaTopology.build();
 	}
 
+    /**
+     * Configures the default values for the 'conf' parameter.
+     * @param conf
+     * @param mongodbUrl
+     */
+    private static void setUpConfig(Config conf, String mongodbUrl){
+        conf.setNumWorkers(1);
+        conf.setMaxSpoutPending(500);
+
+        String partsStr = mongodbUrl.split("://")[1];
+        String[] parts = partsStr.split("/");
+        String[] hostPort = parts[0].split(":");
+
+        conf.put("mongoHost", hostPort[0]);
+        conf.put("mongoPort", Integer.valueOf(hostPort[1]));
+        conf.put("mongoDB", parts[1]);
+    }
+
+    /**
+     *
+     * @param args either [<sessionId>, <mongodbUrl>, 'debug'] (local cluster)
+     *             or [<sessionId>, <mongodbUrl>] for production mode.
+     *             'mongodbUrl' has the following format: 'mongodb://<mongoHost>:<mongoPort>/<mongoDB>'
+     */
 	public static void main(String[] args) {
 
 		Config conf = new Config();
-		conf.setNumWorkers(1);
-		conf.setMaxSpoutPending(500);
+        String sessionId = args[0];
+        setUpConfig(conf, args[1]);
 
-		conf.put("mongoHost", "localhost");
-		conf.put("mongoPort", 27017);
-		conf.put("mongoDB", "lrs");
-
-		if (args.length == 2 && "debug".equals(args[0])) {
+		if (args.length == 3 && "debug".equals(args[2])) {
 			LocalCluster cluster = new LocalCluster();
-			String sessionId = args[1];
 			cluster.submitTopology(sessionId, conf,
 					buildTopology(conf, sessionId));
 		} else {
 			try {
-				String sessionId = args[0];
 				System.out.println("Starting analysis of session " + sessionId);
 				StormSubmitter.submitTopology(sessionId, conf,
 						buildTopology(conf, sessionId));
