6cb03ef95df6952f6bc0645a9072792b11ef7687
RotaruDan <drotaru@ucm.es>
2017-01-23 19:54:17 +0100

    Improves RealtimeTopology documentation

--- src/main/java/es/eucm/rage/realtime/topologies/RealtimeTopology.java
+++ src/main/java/es/eucm/rage/realtime/topologies/RealtimeTopology.java
@@ -45,6 +45,16 @@ public class RealtimeTopology extends TridentTopology {
 
 		/** ---> Analysis definition <--- **/
 
+		/*
+		 * --> Analyzing for Kibana visualizations (traces index, 'sessionId')
+		 * <--
+		 */
+
+		// 1 - For each "trace" (from Kibana) that we receive
+		// 2 - Create an ElasticSearch "sanitized" document identified as
+		// "document"
+		// 3 - Finally persist the "document" to the "sessionId" ElasticSearch
+		// index
 		tracesStream.each(
 				new Fields("trace"),
 				new DocumentBuilder(elasticStateFactory.getConfig()
@@ -52,11 +62,21 @@ public class RealtimeTopology extends TridentTopology {
 				.partitionPersist(elasticStateFactory, new Fields("document"),
 						new TraceStateUpdater());
 
+		/*
+		 * --> Analyzing for the Alerts and Warnings system (results index,
+		 * 'results-sessionId') <--
+		 */
+
+		// 1 - For each "trace" (from Kibana) that we receive
+		// 2 - Extract the fields "gameplayId" and "event" so that we can play
+		// with it below
 		Stream gameplayIdStream = tracesStream.each(new Fields("trace"),
 				new TraceFieldExtractor("gameplayId", "event"), new Fields(
 						"gameplayId", "event"));
 
-		// Timestamp of the last trace per gameplayId
+		// 3 - For each "trace" (from Kibana) that we receive
+		// 4 - Extract the field 'timestamp' and add it to the document per
+		// 'gameplayId' (player)
 		gameplayIdStream
 				.each(new Fields("trace"),
 						new TraceFieldExtractor("timestamp"),
@@ -68,7 +88,7 @@ public class RealtimeTopology extends TridentTopology {
 						new Fields("versionId", "gameplayId", "p", "v"),
 						gameplayStateUpdater);
 
-		// Name of the given gameplayId
+		// 5 - Add the name of the given player to the document ('gameplayId')
 		gameplayIdStream
 				.each(new Fields("trace"), new TraceFieldExtractor("name"),
 						new Fields("name"))
@@ -80,6 +100,9 @@ public class RealtimeTopology extends TridentTopology {
 						gameplayStateUpdater);
 
 		// Alternatives (selected & unlocked) processing
+		// 6 - For each "selected" or "unlocked" trace (alternative type)
+		// 7 - Count the amount of different "responses" have been made (Group
+		// By "response")
 		gameplayIdStream
 				.each(new Fields("event", "trace"),
 						new FieldValuesOrFilter("event", "selected", "unlocked"))
@@ -97,6 +120,10 @@ public class RealtimeTopology extends TridentTopology {
 
 		// Accessible (accessed & skipped)), GameObject (interacted & used) and
 		// Completable (initialized) processing
+		// 8 - For each "accessed", "skipped" (Accessible), "initialized"
+		// (Completable), "interacted or "used" (GameObject) trace
+		// 9 - Count the amount of different "targets" have been made (Group By
+		// "target")
 		gameplayIdStream
 				.each(new Fields("event", "trace"),
 						new FieldValuesOrFilter("event", "accessed", "skipped",
@@ -114,6 +141,8 @@ public class RealtimeTopology extends TridentTopology {
 						new Fields("count"));
 
 		// Completable (Progressed) processing
+		// 10 - For each "progressed" (Completable) trace
+		// 11 - Update the "progress" field to its latest value
 		gameplayIdStream
 				.each(new Fields("event", "trace"),
 						new FieldValueFilter("event", "progressed"))
@@ -129,6 +158,8 @@ public class RealtimeTopology extends TridentTopology {
 						gameplayStateUpdater);
 
 		// Completable (Completed) processing for field "success"
+		// 10 - For each "progressed" (Completable) trace
+		// 11 - Update the "success" field to its latest value
 		gameplayIdStream
 				.each(new Fields("event", "trace"),
 						new FieldValueFilter("event", "completed"))
@@ -144,6 +175,8 @@ public class RealtimeTopology extends TridentTopology {
 						gameplayStateUpdater);
 
 		// Completable (Completed) processing for field "score"
+		// 10 - For each "progressed" (Completable) trace
+		// 11 - Update the "score" field to its latest value
 		gameplayIdStream
 				.each(new Fields("event", "trace"),
 						new FieldValueFilter("event", "completed"))
@@ -156,6 +189,11 @@ public class RealtimeTopology extends TridentTopology {
 				.partitionPersist(elasticStateFactory,
 						new Fields("versionId", "gameplayId", "p", "v"),
 						gameplayStateUpdater);
+
+		/*
+		 * --> Additional/custom analysis needed can be added here or changing
+		 * the code above <--
+		 */
 
 	}
 
